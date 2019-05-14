057a049088ffed75b7bfc983bf4d4c078351eacb
Essux <jjsuarestra99@gmail.com>
2019-05-14 00:14:29 -0500

    Fixing SparseMatrix construct

--- SparseMatrix.cpp
+++ SparseMatrix.cpp
@@ -7,3 +7,8 @@ int SparseMatrix::get_n_cols() {
 int SparseMatrix::get_n_rows() {
     return n_rows;
 }
+
+SparseMatrix::SparseMatrix(int n_rows_, int n_cols_) {
+    n_rows = n_rows_;
+    n_cols = n_cols_;
+}
