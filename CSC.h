87c2d39de431d44731f151462d368031ee1d5757
TheBaxes <sebas1475@gmail.com>
2019-05-27 17:01:56 -0500

    Remove CSC leak

--- CSC.h
+++ CSC.h
@@ -10,8 +10,6 @@ class CSC : public SparseMatrix {
   DenseMatrix to_dense();
   void print_matrix();
   vector<double> mul(vector<double>);
-  vector<double> jacobi_method(vector<double> b, double tol, int maxiter);
-  vector<double> gauss_seidel_method(vector<double> b, double w, double tol, int maxiter);
   
   CSC(int n_rows_, int n_cols_);
 };
