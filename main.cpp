20bdd64c4c0843cc8e1953e20eafc7642c793b09
Essux <jjsuarestra99@gmail.com>
2019-05-26 23:55:42 -0500

    Adding Matrix Generator

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include "COO.h"
 #include "CSR.h"
 #include "CSC.h"
+#include "Generator.h"
 #include <iostream>
 #include <cstdio>
-#include <stdlib.h>
-#include <time.h>
 
 using namespace std;
-
-double urand() {
-    return (double) rand() / RAND_MAX;
-}
 
 void print_vector(vector<double> vec) {
     for (int i = 0; i < vec.size(); i++) printf("%.2f ", vec[i]);
@@ -20,30 +15,25 @@ void print_vector(vector<double> vec) {
 #define MAT_TYPE COO
 
 int main() {
-    int N = 3;
-    vector<double> arr(N*N, 0.0);
-    for (int i = 0; i < N*N; i++) {
-        cin >> arr[i];
-    }
+    int N; cout << "Enter matrix dimension: ";
+    cin >> N;
 
-    DenseMatrix a(arr, N, N);
-    printf("Dense Matrix\n");
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%3.2f ", a.get_pos(i, j));
-        }
-        cout << '\n';
-    }
-    MAT_TYPE coef_mat = MAT_TYPE::from_dense(a);
+    Generator gen(0, 100);
+    DenseMatrix A = gen.generate_diag_dominant(N, N, 1);
+    A.print_matrix();
+    vector<double> x_real = gen.generate_vector(N);
+    vector<double> b = gen.generate_system(A, x_real);
+    printf("Vector B\n");
+    print_vector(b);
 
-    vector<double> b(N, 0);
-    for (int i = 0; i < N; i++) {
-        cin >> b[i];
-    }
+    MAT_TYPE A_sparse = MAT_TYPE::from_dense(A);
+
     try {
-        printf("X Vector\n");
-        vector<double> x = coef_mat.gauss_seidel_method(b, 1e-8, 40);
-        print_vector(x);
+        vector<double> x_aprox = A_sparse.gauss_seidel_method(b, 1e-5, 20);
+        printf("X Vector Aprox\n");
+        print_vector(x_aprox);
+        printf("X Vector Real\n");
+        print_vector(x_real);
     } catch (char const* e) {
         cout << e << endl;
     }
