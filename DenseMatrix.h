1f1bda0678b9a90a35dbaf6da3363a604dc659d4
Juan José Suárez <jjsuarestra99@gmail.com>
2019-05-26 22:21:08 -0500

    Dense Methods for Solving Systems of Linear Equations (#21)
    * Adding Matrix-Vector multiplication
    
    * Adding Jacobi method
    
    * Adding Gauss-Seidel method
    
    * Adding Gaussian Elimination

--- DenseMatrix.h
+++ DenseMatrix.h
@@ -10,6 +10,21 @@ class DenseMatrix {
     public:
         int get_n_rows();
         int get_n_cols();
+        void print_matrix();
         double get_pos(int row, int col);
+        void set_pos(int row, int col, double val);
         DenseMatrix(vector<double> mat_, int rows_, int cols_);
+        DenseMatrix(int rows_, int cols);
+
+        vector<double> mul(vector<double> x);
+        vector<double> jacobi_method(vector<double> b, double tolerance, int iterations);
+        vector<double> jacobi_iteration(vector<double> x0, vector<double> b);
+        vector<double> gauss_seidel_method(vector<double> b, double tolerance, int iterations);
+        vector<double> gauss_seidel_iteration(vector<double> x0, vector<double> b);
+        DenseMatrix gaussian_elimination(vector<double> b);
+
+        DenseMatrix augment_matrix(vector<double> b);
+        private:
+        double infinite_norm(vector<double> x0);
+        double measure_dispersion(vector<double> x0, vector<double> x1);
 };
