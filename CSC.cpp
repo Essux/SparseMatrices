d4f2f2ba2f7b3e6f19a8fb817a502e1a30f272bb
Sebastian Patiño <sebas1475@gmail.com>
2019-05-27 21:43:33 -0500

    CSC Methods for Solving Systems of Linear Equations (#27)
    * Add jacobi
    
    * Add gauss seidel and fixed jacobi
    
    * Fix Jacobi and Gauss implementations
    
    * Delete whitespaces

--- CSC.cpp
+++ CSC.cpp
@@ -1,6 +1,7 @@
 #include "CSC.h"
 #include <vector>
 #include <iostream>
+#include <cmath>
 
 CSC CSC::from_dense(DenseMatrix mat) {
   CSC repr(mat.get_n_rows(), mat.get_n_cols());
@@ -51,6 +52,105 @@ vector<double> CSC::mul(vector<double> x) {
     R[IA[j]] += x[i] * AA[j];
   }
   return R;
+}
+
+vector<double> CSC::jacobi_method(vector<double> b, double tol, int maxiter) {
+  int nz = AA.size();
+  int ncol = get_n_cols();
+  vector<double> x(ncol, 0);
+  vector<double> x0(ncol, 0);
+  int k = 0;
+  double err = tol + 1;
+  vector<double> diag(ncol, 0);
+  while (err > tol and k < maxiter) {
+    for (int i = 0; i < ncol; ++i) {
+      x[i] = 0;
+    }
+    for (int i = 0; i < ncol; ++i) {
+      for (int j = JA[i]; j < JA[i+1]; ++j){
+	if (i != IA[j]) {
+	  x[IA[j]] += AA[j] * x0[i];
+	} else {
+	  diag[i] = AA[j];
+	}
+      }
+    }
+    for (int i = 0; i < ncol; ++i) {
+      x[i] = (b[i] - x[i]) / diag[i];
+    }
+    err = abs(x[0] - x0[0]); //  norm(x-x0);
+    for (int i = 0; i < x.size(); ++i) {
+      if (abs(x[i] - x0[i]) > err) {
+	err = abs(x[i] - x0[i]);
+      }
+    }
+    double div = x[0];
+    for (int i = 0; i < x.size(); ++i) {
+      if (x[i] > div) {
+	div = x[i];
+      }
+    }
+    err /= div;
+    x0 = x;
+    k++;
+  }
+  if (k < maxiter) {
+    return x;
+  }
+  throw "rip";
+}
+
+vector<double> CSC::successive_over_relaxation(vector<double> b, double w, double tol, int maxiter) {
+  int nz = AA.size();
+  int ncol = b.size();
+  vector<double> x(ncol, 0);
+  vector<double> x0(ncol, 0);
+  int k = 0;
+  double err = tol + 1;
+  vector<double> diag(ncol, 0);
+  while (err > tol and k < maxiter) {
+    for (int i = 0; i < ncol; ++i) {
+      x[i] = 0;
+      for (int j = 0; j < JA.size()-1; ++j) {
+        for (int k = JA[j]; k < JA[j+1]; ++k) {
+	  if (i == IA[k]) {
+	    if (i < j) {
+	      x[i] += AA[k] * x0[j];
+	    } else if (i > j) {
+	      x[i] += AA[k] * x[j];
+	    } else {
+	      diag[i] = AA[k];
+	    }
+	  }
+	}
+      }
+      x[i] = (b[i] - x[i]) / diag[i];
+      x[i] = w*x[i] + (1-w)*x0[i];
+    }
+    err = abs(x[0] - x0[0]); //  norm(x-x0);
+    for (int i = 0; i < x.size(); ++i) {
+      if (abs(x[i] - x0[i]) > err) {
+	err = abs(x[i] - x0[i]);
+      }
+    }
+    double div = x[0];
+    for (int i = 0; i < x.size(); ++i) {
+      if (x[i] > div) {
+	div = x[i];
+      }
+    }
+    err /= div;
+    x0 = x;
+    k++;
+  }
+  if (k < maxiter) {
+    return x;
+  }
+  throw "rip";
+}
+
+vector<double> CSC::gauss_seidel_method(vector<double> b, double tol, int maxiter) {
+  return successive_over_relaxation(b, 1.0, tol, maxiter);
 }
 
 CSC::CSC(int n_rows_, int n_cols_) : SparseMatrix::SparseMatrix(n_rows_, n_cols_) {}
--- CSC.h
+++ CSC.h
@@ -10,6 +10,9 @@ class CSC : public SparseMatrix {
   DenseMatrix to_dense();
   void print_matrix();
   vector<double> mul(vector<double>);
+  vector<double> jacobi_method(vector<double> b, double tol, int maxiter);
+  vector<double> successive_over_relaxation(vector<double> b, double w, double tol, int maxiter);
+  vector<double> gauss_seidel_method(vector<double> b, double tol, int maxiter);
   
   CSC(int n_rows_, int n_cols_);
 };
